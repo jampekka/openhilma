ae33e0cfdfc13c22cfc7c22f5c7ab492d7cbd493
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2015-01-27 23:38:54 +0200

    A quick and dirty UI

--- hilma_rest_api.py
+++ hilma_rest_api.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+from flask import Flask
 from eve import Eve
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.serving import run_simple
 
-app = Eve()
+api = Eve()
+static = Flask(__name__)
+app = DispatcherMiddleware(static, {'/api': api})
 
 if __name__ == '__main__':
-    app.run(port=23058)
+    run_simple('0.0.0.0', 23058, app, use_reloader=True, use_debugger=True)
